44ea64ba3ee45c174fa874db15c76696f03f3592
Oleg Kalandarashvili <V1RTUOZ@users.noreply.github.com>
2020-04-18 21:58:13 +0300

    Update patrons.js

--- patrons.js
+++ patrons.js
@@ -3,6 +3,6 @@ window.patrons = [
         name: "Rift",
         picture: "https://c8.patreon.com/2/200/33630355",
         social: null,
-        link: null
+        link: "http://ya.ru"
     }
 ];
