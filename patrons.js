504a5418778f7ae388bf58deab7357f6d055fd66
Oleg Kalandarashvili <V1RTUOZ@users.noreply.github.com>
2020-04-29 10:56:53 +0300

    Update patrons.js

--- patrons.js
+++ patrons.js
@@ -8,7 +8,7 @@ window.patrons = [
     },
     {
         name: "MrSquid",
-        donation: "5$🤟",
+        donation: "5$🥴",
         picture: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/a1/a146f1f5ac5d68307a93f0382e8a23cc2603c03a_full.jpg",
         social: "Steam",
         link: "https://steamcommunity.com/id/mrsquiid"
