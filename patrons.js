e7d79e30f9cd8befdaca24ef986159a497a7d32b
Oleg Kalandarashvili <V1RTUOZ@users.noreply.github.com>
2020-04-29 09:12:46 +0300

    Update patrons.js

--- patrons.js
+++ patrons.js
@@ -4,5 +4,11 @@ window.patrons = [
         picture: "https://c8.patreon.com/2/200/33630355",
         social: null,
         link: null
+    },
+    {
+        name: "MrSquid",
+        picture: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/a1/a146f1f5ac5d68307a93f0382e8a23cc2603c03a_full.jpg",
+        social: "Steam",
+        link: "https://steamcommunity.com/id/mrsquiid"
     }
 ];
