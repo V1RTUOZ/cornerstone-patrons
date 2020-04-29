ceaa1e981f867d54e41701f115ef2fa3dd6ea9d9
Oleg Kalandarashvili <V1RTUOZ@users.noreply.github.com>
2020-04-29 10:17:19 +0300

    Update patrons.js

--- patrons.js
+++ patrons.js
@@ -1,14 +1,14 @@
 window.patrons = [
     {
         name: "Rift",
-        donation: "10💵",
+        donation: "10💲",
         picture: "https://c8.patreon.com/2/200/33630355",
         social: null,
         link: null
     },
     {
         name: "MrSquid",
-        donation: "5💵🔥",
+        donation: "5💲🔥",
         picture: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/a1/a146f1f5ac5d68307a93f0382e8a23cc2603c03a_full.jpg",
         social: "Steam",
         link: "https://steamcommunity.com/id/mrsquiid"
